8377fda03f01980ad2232adb3b4b65ea7b993f9e
ft <ft@noemail>
2017-03-26 10:46:27 +0200

    finalized GUI application

--- SilverSim/Main.GUI/GuiApplication.cs
+++ SilverSim/Main.GUI/GuiApplication.cs
@@ -1,13 +1,33 @@
-﻿using SilverSim.Updater;
+﻿// SilverSim is distributed under the terms of the
+// GNU Affero General Public License v3 with
+// the following clarification and special exception.
+
+// Linking this library statically or dynamically with other modules is
+// making a combined work based on this library. Thus, the terms and
+// conditions of the GNU Affero General Public License cover the whole
+// combination.
+
+// As a special exception, the copyright holders of this library give you
+// permission to link this library with independent modules to produce an
+// executable, regardless of the license terms of these independent
+// modules, and to copy and distribute the resulting executable under
+// terms of your choice, provided that you also meet, for each linked
+// independent module, the terms and conditions of the license of that
+// module. An independent module is a module which is not derived from
+// or based on this library. If you modify this library, you may extend
+// this exception to your version of the library, but you are not
+// obligated to do so. If you do not wish to do so, delete this
+// exception statement from your version.
+
+using Microsoft.Win32;
+using SilverSim.Updater;
 using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace SilverSim.Main.GUI
@@ -29,7 +49,7 @@ namespace SilverSim.Main.GUI
         {
             m_Args = args;
             m_TrayMenu = new ContextMenu();
-            m_TrayMenu.MenuItems.Add("Exit", OnExit);
+            m_TrayMenu.MenuItems.Add("Shutdown application", OnExit);
 
             m_TrayIcon = new NotifyIcon();
             m_TrayIcon.Text = "SilverSim";
@@ -76,10 +96,18 @@ namespace SilverSim.Main.GUI
             pi.SetMethod.Invoke(m_Startup, new object[] { true });
             MethodInfo mi = t.GetMethod("Run");
             Action<string> del = OnConsoleWrite;
+            SystemEvents.SessionEnded += OnSessionEnded;
             if (!(bool)mi.Invoke(m_Startup, new object[] { m_Args, del }))
             {
                 Thread.Sleep(3000);
             }
+            SystemEvents.SessionEnded -= OnSessionEnded;
+        }
+
+        void OnSessionEnded(object sender, SessionEndedEventArgs e)
+        {
+            TriggerShutdown();
+            Application.Exit();
         }
 
         protected override void OnLoad(EventArgs e)
@@ -93,17 +121,17 @@ namespace SilverSim.Main.GUI
         {
             m_TrayIcon.BalloonTipTitle = "SilverSim";
             m_TrayIcon.BalloonTipText = msg;
-            m_TrayIcon.ShowBalloonTip(1000);
+            m_TrayIcon.ShowBalloonTip(10000);
         }
 
         void OnCoreUpdaterLog(CoreUpdater.LogType type, string message)
         {
-            Console.WriteLine("Updater - [{0}] - {1}", type.ToString(), message);
+            m_TrayIcon.BalloonTipTitle = "SilverSim";
+            m_TrayIcon.BalloonTipText = string.Format("Updater: [{0}] - {1}", type.ToString(), message);
+            m_TrayIcon.ShowBalloonTip(10000);
         }
 
-
-
-        void OnExit(object sender, EventArgs e)
+        void TriggerShutdown()
         {
             if (null != m_Startup)
             {
@@ -111,6 +139,11 @@ namespace SilverSim.Main.GUI
                 MethodInfo mi = t.GetMethod("Shutdown");
                 mi.Invoke(m_Startup, new object[0]);
             }
+        }
+
+        void OnExit(object sender, EventArgs e)
+        {
+            TriggerShutdown();
             Application.Exit();
         }
 
@@ -118,7 +151,6 @@ namespace SilverSim.Main.GUI
         {
             if (isDisposing)
             {
-                // Release the icon resource.
                 m_TrayIcon.Dispose();
             }
 
